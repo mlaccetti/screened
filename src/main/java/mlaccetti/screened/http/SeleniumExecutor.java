5894298c7d05ddfa9e6a9b9498cda01d9dd995eb
Michael Laccetti <michael@laccetti.com>
2022-01-19 16:47:24 -0500

    all sorts of updates and made work again

--- src/main/java/mlaccetti/screened/http/SeleniumExecutor.java
+++ src/main/java/mlaccetti/screened/http/SeleniumExecutor.java
@@ -1,11 +1,12 @@
 package mlaccetti.screened.http;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.java.Log;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.boot.system.ApplicationTemp;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.*;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -24,75 +26,60 @@ public class SeleniumExecutor {
   private ApplicationTemp appTemp;
 
   public void downloadResults() throws Exception {
-    final String downloadFilepath = appTemp.getDir().getAbsolutePath();
-    final Map<String, Object> chromePrefs = new HashMap<>();
-    chromePrefs.put("profile.default_content_settings.popups", 0);
-    chromePrefs.put("download.default_directory", downloadFilepath);
+    WebDriverManager.edgedriver().setup();
 
-    final ChromeOptions options = new ChromeOptions();
-    options.setExperimentalOption("prefs", chromePrefs);
+    final String downloadFilepath = appTemp.getDir().getAbsolutePath();
+    final Map<String, Object> edgePrefs = new HashMap<>();
+    edgePrefs.put("profile.default_content_settings.popups", 0);
+    edgePrefs.put("download.default_directory", downloadFilepath);
+
+    final EdgeOptions options = new EdgeOptions();
+    options.setExperimentalOption("prefs", edgePrefs);
     options.addArguments("--headless", "--disable-gpu", "--disable-crash-reporter");
 
-    final WebDriver driver = new ChromeDriver(options);
-    final WebDriverWait wait = new WebDriverWait(driver, 10);
+    final WebDriver driver = new EdgeDriver(options);
+    final WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
     driver.get("https://covid-19.ontario.ca/school-screening/");
 
-    final WebElement startScreeningButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()=\"Start school screening\"]")));
+    final WebElement startScreeningButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Start school screening')]")));
     log.info("Clicking 'start school screening' button.");
     startScreeningButton.click();
-
-    final WebElement guardianRadioButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='guardian']")));
-    log.info("Selecting 'guardian' input for 'taking screen as'.");
-    guardianRadioButton.click();
-
-    final WebElement guardianContinueButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Continue')]")));
-    log.info("Clicking 'guardian' continue button.");
-    guardianContinueButton.click();
     wait.until(ExpectedConditions.urlContains("/vaccinated"));
 
-    final WebElement vaccinatedOrPositive = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
-    log.info("Clicking 'Fully vaccinated or tested positive' no button.");
-    vaccinatedOrPositive.click();
+    final WebElement under11Button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Yes')]")));
+    log.info("Clicking 'yes' to under 11.");
+    under11Button.click();
     wait.until(ExpectedConditions.urlContains("/travel"));
 
-    final WebElement travelledOutsideOfCanadaButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
-    log.info("Clicking 'no' to travelling outside of Canada.");
-    travelledOutsideOfCanadaButton.click();
-    wait.until(ExpectedConditions.urlContains("/doctor-self-isolate"));
+    final WebElement internationalTravelButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
+    log.info("Clicking 'no' to international travel.");
+    internationalTravelButton.click();
+    wait.until(ExpectedConditions.urlContains("/symptoms"));
+
+    final WebElement noSymptomsButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Continue')]")));
+    log.info("Clicking 'continue' for symptoms.");
+    noSymptomsButton.click();
+    wait.until(ExpectedConditions.urlContains("/covid-positive"));
+
+    final WebElement last5DaysPositive = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
+    log.info("Clicking 'no' to last 5 days tested positive.");
+    last5DaysPositive.click();
+    wait.until(ExpectedConditions.urlContains("/household-isolation"));
 
     final WebElement currentlyIsolatingButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
     log.info("Clicking 'no' to should be currently isolating.");
     currentlyIsolatingButton.click();
+    wait.until(ExpectedConditions.urlContains("/doctor-self-isolate"));
+
+    final WebElement doctorIsolatingButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
+    log.info("Clicking 'no' to being told to isolate.");
+    doctorIsolatingButton.click();
     wait.until(ExpectedConditions.urlContains("/contact"));
 
     final WebElement closeContactButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
-    log.info("Clicking 'no' to being in close contact with Covid button.");
+    log.info("Clicking 'no' to being a close contact.");
     closeContactButton.click();
-    wait.until(ExpectedConditions.urlContains("/covid-alert"));
-
-    final WebElement covidAlertButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
-    log.info("Clicking 'no' to having a covid alert notification button.");
-    covidAlertButton.click();
-    wait.until(ExpectedConditions.urlContains("/covid-alert"));
-
-    final WebElement noneOfTheAboveInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='none_of_the_above']")));
-    log.info("Clicking 'none of the above' input.");
-    noneOfTheAboveInput.click();
-
-    final WebElement noneOfTheAboveContinue = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Continue')]")));
-    log.info("Clicking 'none of the above' continue button.");
-    noneOfTheAboveContinue.click();
-    wait.until(ExpectedConditions.urlContains("/covid-test"));
-
-    final WebElement isolatingButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
-    log.info("Clicking 'no' to living with folks currently experiencing symptoms.");
-    isolatingButton.click();
-    wait.until(ExpectedConditions.urlContains("/rapid-test"));
-
-    final WebElement testedPositiveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'No')]")));
-    log.info("Clicking 'no' to tested positive in last ten days.");
-    testedPositiveButton.click();
     wait.until(ExpectedConditions.urlContains("/approved"));
 
     log.info("All buttons clicked, downloading results.");
